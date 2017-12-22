40bfcab7a450867c4e5076420389f18de398e5d5
remco <remcohoeneveld@gmail.com>
2017-12-22 12:41:27 +0100

    cleaned code

--- js/loadArticle.js
+++ js/loadArticle.js
@@ -29,7 +29,6 @@ datepickerContainer.datepicker({
         if (dateAsObject < currentdate) {
             clearCanvas();
             getData(dateAsObject);
-            console.log(dateAsObject)
         }
     }
 });
@@ -37,7 +36,6 @@ datepickerContainer.datepicker({
 
 function formatDateString(MyDate) {
     MyDateString = (MyDate.getFullYear() + '/' + ('0' + (MyDate.getMonth() + 1)).slice(-2) + '/' + ('0' + MyDate.getDate()).slice(-2));
-    console.log(MyDateString);
     return MyDateString
 }
 
